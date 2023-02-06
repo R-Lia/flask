0da11ae1b3425b45bf4a667598fd66433c847033
Drys <bordaure@gmail.com>
2023-02-06 18:40:42 +0100

    First commit

---
+++ memeFl.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+def get_meme():
+    url = "https://meme-api.com/gimme"
+    r = requests.get(url)
+    jsonR = json.loads(r.text)
+    #preview = jsonR["preview"][-1]
+    #author = 
+    return jsonR
+
+@app.route('/')
+def index():
+    jsonR = get_meme()
+    data = {"author":jsonR["author"]}
+    return render_template("index_meme.html",data=jsonR)
+
+
+    
+app.run(host="0.0.0.0", port=5000)
